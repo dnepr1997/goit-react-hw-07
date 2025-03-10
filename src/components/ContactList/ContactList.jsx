b413e3b5aa08e9167e44748b14038f69c831a68f
Olexander <dneprovsky1997@icloud.com>
2025-03-10 19:53:18 +0100

    refactoring

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,30 +1,21 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Contact } from '../Contact/Contact';
 import { deleteContact } from '../../redux/contactsOps';
-import { useEffect } from 'react';
-import { fetchData } from '../../redux/contactsOps';
+
 import { selectError, selectFilteredContacts, selectLoading } from '../../redux/contactsSlice';
-import { selectNameFilter } from '../../redux/filtersSlice';
 
 export const ContactList = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(selectFilteredContacts);
-  const filter = useSelector(selectNameFilter);
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
-  const filterData = contacts.filter(item =>
-    item.name.toLowerCase().includes(filter.toLowerCase())
-  );
-  useEffect(() => {
-    dispatch(fetchData());
-  }, [dispatch]);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
 
   return (
     <ul>
-      {filterData.map(contact => (
+      {contacts.map(contact => (
         <Contact
           key={contact.id}
           contact={contact}
